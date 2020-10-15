3e1389076320cc8058743115da6bafb2af30b340
Alexandre Milani <alexandre.milani00@gmail.com>
2020-10-14 21:14:19 -0400

    Added dependencies.

--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ setup(
     py_modules=['main'],
     install_requires=[
         'click',
+        'bs4',
         'selenium',
+        'tinydb',
     ],
     entry_points='''
         [console_scripts]
